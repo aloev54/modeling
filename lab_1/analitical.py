dba07bd29d79969dcd8461f52ecdbb802f82dbd3
aloev54 <aloev00@gmail.com>
2024-11-20 01:43:34 +0300

    add euler

--- lab_1/analitical.py
+++ lab_1/analitical.py
@@ -3,7 +3,7 @@ import math
 
 def analiticalTask1(x, h, n):
     res = []
-    for i in range(n):
+    for i in range(n+1):
         res.append(3 * math.exp(x) - x ** 2 - 2 * x - 2)
         x += h
     return res
@@ -11,7 +11,7 @@ def analiticalTask1(x, h, n):
 
 def analiticalTask2(x, h, n):
     res = []
-    for i in range(n):
+    for i in range(n+1):
         res.append(math.exp(x ** 2) - (x ** 2) / 2 - 1 / 2)
         x += h
     return res
--- lab_1/main.py
+++ lab_1/main.py
@@ -1,54 +1,50 @@
-
 def interfaceProgramm():
-	stage = 0
+    stage = 0
 
+    print("лабораторна работа №1")
+    print("Выберите номер задания")
+    stage = int(input())
 
-	print("лабораторна работа №1")
-	print("Выберите номер задания")
-	stage = int(input())
-	
-
-	if stage == 1:
-		stage1()
-	elif stage == 2:
-		stage2()
-	elif stage == 3:
-		stage3()
-
+    if stage == 1:
+        stage1()
+    elif stage == 2:
+        stage2()
+    elif stage == 3:
+        stage3()
 
 
 def stage1():
-	print("Программа № 1")
-	print("a - начало интервала, b - конец интервала, h - шаг интервала")
+    print("Программа № 1")
+    print("a - начало интервала, b - конец интервала, h - шаг интервала")
 
-	print("Введите значение a")
-	a = float(input())
+    print("Введите значение a")
+    a = float(input())
 
-	print("Введите значение b")
-	b = float(input())
+    print("Введите значение b")
+    b = float(input())
 
-	print("Введите значение h")
-	h = float(input())
+    print("Введите значение h")
+    h = float(input())
 
-	x = []
-	n = (b-a)/h
-	i = 0
-	while True:
-		if i != n+1:
-			x.append(a+h*i)
-		else:
-			break
-		i +=1
-	print(x)
-
+    x = []
+    n = (b - a) / h
+    i = 0
+    while True:
+        if i != n + 1:
+            x.append(a + h * i)
+        else:
+            break
+        i += 1
+    print(x)
 
 
 def stage2():
-	pass
+    pass
+
 
 def stage3():
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	interfaceProgramm()
+    interfaceProgramm()
