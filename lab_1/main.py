f53cd5156a4c760b8eb24920bf2cb3937084a70c
sergeyMedvedev <sm@zero-ediscovery.ru>
2024-11-20 01:15:16 +0300

    add main.go

---
+++ lab_1/main.py
@@ -0,0 +1,31 @@
+
+
+if __name__== '__main__':
+	pass
+
+def interfaceProgramm():
+	stage = 0
+
+
+	print("лабораторна работа №1")
+	print("Выберите номер задания")
+	stage = int(input())
+	
+
+	if stage == 1:
+		stage1()
+	elif stage == 2:
+		stage2()
+	elif stage == 3:
+		stage3()
+
+
+
+def stage1():
+	pass
+
+def stage2():
+	pass
+
+def stage3():
+	pass
